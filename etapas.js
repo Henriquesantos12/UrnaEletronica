bd8da023e860cf639037e1feaeed2f3f82b7a13f
Henrique Santos <114838533+Henriquesantos12@users.noreply.github.com>
2023-03-29 13:52:15 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,40 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+                {
+                    numero: 38111,
+                    nome: 'Fulano de tal',
+                    partido: 'ABC',
+                    fotos:[
+                        { url:'Prefeito.jpg', legenda: 'Vereador'}
+                    ]
+        },
+        {
+            numero: 77222,
+            nome: 'Beltrano da Silva',
+            partido: 'DEFG',
+            fotos: [
+                {url: 'vice.png', legenda: 'Vereador'}
+            ]
+    },
+]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {url: 'Prefeito.jpg', legenda: 'Prefeito' },
+                    {url: 'vice.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
---
+++ script.js
@@ -0,0 +1,40 @@
+let seuVotoPara = document.querySelector(".d-1-1 span");
+let cargo = document.querySelector(".d-1-2 span");
+let descrição = document.querySelector(".d-1-4");
+let aviso = document.querySelector("d-2");
+let lateral = document.querySelector(".d-1-right");
+let numeros = document.querySelector("d-1-3");
+
+let etapaAtual = 0;
+
+function comecarEtapa() {
+    let etapa = etapas[etapaAtual];
+    
+    let numeroHtml= '';
+
+    for(let i=0;i<etapa.numeros;i++) {
+        numeroHtml += '<div class="numero"></div>';
+    }
+    
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descrição.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+}
+
+function clicou(n) {
+    alert('Clicou em' + n)
+}
+function branco() {
+    alert('Clicou em BRANCO')
+} 
+function corrige() {
+    alert('Clicou em CORRIGE')
+}
+function confirma() {
+    alert('Clicou em CONFIRMA')
+}
+
+comecarEtapa();
